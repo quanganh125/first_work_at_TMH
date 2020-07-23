5480e73fd90e3d2c9a293f90131dc022e0b05d9d
quanganh <quanganhk62@gmail.com>
2020-07-23 11:58:37 +0700

    add Controller login

--- training/cgi-bin/src/Controller/UserController.php
+++ training/cgi-bin/src/Controller/UserController.php
@@ -4,10 +4,23 @@ namespace App\Controller;
 
 class UserController extends AppController{
     public function login(){
+        $this->loadModel('tUsers');
+        $login = $this->tUsers->newEmptyEntity();
+        $session = $this->request->getSession();
 
+        if($this->request->is('post')){
+            $login->email = $this->request->getData('email');  
+            $login->password = $this->request->getData('password');   
+
+            if($this->tUsers->find()->where(['email' => $login->email, 'login'=>$login->password]))
+                $session->write([
+                    'User.email' => $login->email,
+                    'User.password' => $login ->password
+                ]);
+        }
     }
 
     public function regist(){
-        
+
     }
 }
